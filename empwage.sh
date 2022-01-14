8e71f072817d758ff2db5b0a1713fe116760c053
Rushi <189489rishi@gmail.com>
2022-01-14 15:01:23 +0530

    empwage

---
+++ empwage.sh
@@ -0,0 +1,13 @@
+
+ispresent=1;
+  randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+empRatePerHrs = 20;
+empHrs = 8;
+Salary = $(($empRatePerHrs*$empHrs));
+   echo $salary
+else
+    salary =0;
+echo $salary;
+fi
