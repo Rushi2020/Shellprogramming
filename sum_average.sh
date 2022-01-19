2838ad63de9a101a993306a696ec3efb91ed7aa7
Rushi <189489rishi@gmail.com>
2022-01-19 20:09:19 +0530

    Added: sequence sum and average

---
+++ sum_average.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter number:"
+read a
+sum=0
+echo "$a Random generated numbers"
+for (( i=a; i >= 1; i-- ));
+do
+	b=`echo $((RANDOM%10))`
+	echo $b
+	sum=$((sum+b))
+done
+echo "Sum = "$sum "and Avarage = "$((sum/a))
