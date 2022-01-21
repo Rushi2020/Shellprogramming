9667e3c0033a388c48370cb2000bb4d9630f7d2e
Rushi <189489rishi@gmail.com>
2022-01-21 10:39:19 +0530

    Added:sequence pp

---
+++ sequence.sh
@@ -0,0 +1,8 @@
+# Add two random dice number and print result.
+
+#!/bin/bash
+
+one_dice=`echo $((RANDOM%6))`; 
+sec_dice=`echo $((RANDOM%6))`; 
+echo $one_dice; echo $sec_dice; 
+echo $((one_dice+sec_dice))
