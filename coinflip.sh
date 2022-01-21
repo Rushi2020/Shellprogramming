aa8356c005397bdfb238cc1920d22cc65016583d
Rushi <189489rishi@gmail.com>
2022-01-21 10:46:43 +0530

    Added: coin flip

--- coinflip.sh
+++ coinflip.sh
@@ -1,11 +1,38 @@
-#!/bin/bash
+#!/bin/bash +x
 
-i=$((RANDOM%2))
+#Extend the Flip Coin problem till either Heads or Tails wins 11 times.
 
-echo $i
-if [ $i -eq 1 ]
+head=1
+tail=1
+
+while true
+do
+	FLIP=$(($RANDOM%2))
+	
+	case $FLIP in
+		0)
+			if [[ $head -eq 11 ]] || [[ $tail -eq 11 ]]
+			then
+				break
+			else
+				echo "HEADS" $head $FLIP
+				head=$((head+1))
+			fi;;
+			
+		1)
+			if [[ $head -eq 11 ]] || [[ $tail -eq 11 ]]
+			then
+				break
+			else
+				echo "TAILS" $tail $FLIP
+				tail=$((tail+1))
+			fi;;
+	esac
+done
+
+if [ $head -gt $tail ]
 then
-	echo "Heads"
+	echo "HEADS Won"
 else
-	echo "Tails"
+	echo "TAILS Won"
 fi
